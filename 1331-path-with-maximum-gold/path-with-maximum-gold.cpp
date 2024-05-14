409dc9870b25e4729b282afdb7bd9e69c56aa000
Amrit Srivastava <amrit.madhav.srivastava@gmail.com>
2024-05-14 11:26:32 +0530

    Time: 101 ms (83.68%) | Memory: 9 MB (94.87%) - LeetSync

---
+++ 1331-path-with-maximum-gold/path-with-maximum-gold.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int getMaximumHelper(std::vector<std::vector<int>>& grid, int x, int y) {
+        if (x < 0 || x >= grid[0].size() || y < 0 || y >= grid.size() || grid[y][x] == 0) return 0;
+        
+        int gold = grid[y][x];
+        grid[y][x] = 0;
+        
+        int maxGold = 0;
+        maxGold = max(maxGold, gold + getMaximumHelper(grid, x+1, y));
+        maxGold = max(maxGold, gold + getMaximumHelper(grid, x-1, y));
+        maxGold = max(maxGold, gold + getMaximumHelper(grid, x, y+1));
+        maxGold = max(maxGold, gold + getMaximumHelper(grid, x, y-1));
+        
+        grid[y][x] = gold;
+        
+        return maxGold;
+    }
+
+    int getMaximumGold(vector<vector<int>>& grid) {
+        int maxGold = 0;
+        for (int y = 0; y < grid.size(); ++y) {
+            for (int x = 0; x < grid[0].size(); ++x) {
+                maxGold = std::max(maxGold, getMaximumHelper(grid, x, y));
+            }
+        }
+        return maxGold;
+    }
+};
