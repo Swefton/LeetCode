9ef131d28a2942c74d180dcf36967f299d2359e9
Amrit Srivastava <amrit.madhav.srivastava@gmail.com>
2024-05-19 16:31:02 +0530

    Time: 207 ms (35.11%) | Memory: 130.6 MB (32.89%) - LeetSync

--- 3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.cpp
+++ 3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.cpp
@@ -2,25 +2,26 @@ class Solution {
 public:
     long long maximumValueSum(vector<int>& nums, int k, vector<vector<int>>& edges) {
         int n = nums.size();
-        vector<vector<long long>> temp(n, vector<long long>(2, -1));
-        
-        return calculateMax(nums, n, k, 0, 1, temp);
-    }
-
-private:
-    long long calculateMax(vector<int>& nums, int n, int k, int curInd, int isEven, vector<vector<long long>>& temp) {
-        if (curInd == n) {
-            return isEven == 1 ? 0 : LLONG_MIN;
+        vector<long long> deltas(n);
+        for (int i = 0; i < n; i++) {
+            deltas[i] = (nums[i] ^ k) - nums[i];
         }
-        if (temp[curInd][isEven] != -1) {
-            return temp[curInd][isEven];
+        sort(deltas.rbegin(), deltas.rend());
+
+        long long res = 0;
+        for (int num : nums) {
+            res += num;
         }
 
-        long long noXor = nums[curInd] + calculateMax(nums, n, k, curInd + 1, isEven, temp);
-        long long withXor = (nums[curInd] ^ k) + calculateMax(nums, n, k, curInd + 1, !isEven, temp);
+        for (int startInd = 0; startInd < n - 1; startInd += 2) {
+            long long changingDelta = deltas[startInd] + deltas[startInd + 1];
+            if (changingDelta > 0) {
+                res += changingDelta;
+            } else {
+                break;
+            }
+        }
 
-        long long mxPossible = max(noXor, withXor);
-        temp[curInd][isEven] = mxPossible;
-        return mxPossible;
+        return res;
     }
 };
