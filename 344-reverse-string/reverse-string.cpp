a6d655da29cdf9be48c41e771b09669fb1d762eb
Amrit Srivastava <amrit.madhav.srivastava@gmail.com>
2024-06-02 16:23:04 +0530

    Time: 17 ms (57.81%) | Memory: 27.1 MB (21.89%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        auto start = s.begin();
-        auto end = s.end() - 1;
-
-        while (start <= end) iter_swap(start++, end--);
-
+        reverse(s.begin(), s.end());
     }
 };
