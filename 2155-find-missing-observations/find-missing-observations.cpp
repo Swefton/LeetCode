100d0f94001b02eaa2e67b172e198e5e86741d97
Amrit Srivastava <amrit.madhav.srivastava@gmail.com>
2024-09-05 20:06:03 -0400

    Time: 97 ms (59.85%) | Memory: 116.3 MB (69.59%) - LeetSync

---
+++ 2155-find-missing-observations/find-missing-observations.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> missingRolls(vector<int>& rolls, int mean, int n) {
+        int givenSum = 0;
+        for (int roll : rolls) givenSum += roll;
+
+        int totalRolls = n + rolls.size();
+        int missingSum = mean * totalRolls - givenSum;
+
+        if (n * 6 < missingSum || missingSum < n) return {};
+
+        int remainder = missingSum % n;
+        int quotient = missingSum / n;
+
+        vector<int> result(n, quotient);
+        for (int i = 0; i < remainder; i++)
+            result[i]++;
+
+        return result;
+    }
+};
