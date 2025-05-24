ec9a5d8bfbe878d78c09ff5b1b9e94f376032991
Amrit Srivastava <amrit.madhav.srivastava@gmail.com>
2025-05-23 20:04:29 -0400

    Time: 13 ms (7.11%) | Memory: 18 MB (20.62%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.py
@@ -0,0 +1,31 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        result = 0
+        
+        vals = OrderedDict([
+                ('I', 1),
+                ('V', 5),
+                ('X', 10),
+                ('L', 50),
+                ('C', 100),
+                ('D', 500),
+                ('M', 1000)
+            ])
+        
+        i, char = 0, s[0]
+        while i < len(s):
+            if i < len(s)-1 and vals[char] < vals[s[i+1]]:
+                result += vals[s[i+1]] - vals[char]
+                i += 2
+            else:
+                result += vals[s[i]]
+                i += 1
+            
+            if i < len(s):
+                char = s[i]
+
+
+
+
+        return result
+
