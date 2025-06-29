7699e28bd96806d6a17ae9431594aecfeb5a8fe4
Amrit Srivastava <amrit.madhav.srivastava@gmail.com>
2025-06-28 22:17:04 -0400

    Time: 7 ms (18.68%) | Memory: 18.5 MB (52.78%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -3,23 +3,19 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        markers = list()
-        visited_row, visited_col = set(), set()
+        rows = set()
+        cols = set()
 
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
                 if matrix[i][j] == 0:
-                    markers.append((i,j))
+                    rows.add(i)
+                    cols.add(j)
 
-        print(markers)
+        for row in rows:
+            for j in range(len(matrix[0])):
+                matrix[row][j] = 0
 
-        for marker in markers:
-            if marker[0] not in visited_row:
-                for i in range(len(matrix[0])):
-                    matrix[marker[0]][i] = 0
-            if marker[1] not in visited_col:
-                for i in range(len(matrix)):
-                    matrix[i][marker[1]] = 0
-
-            visited_row.add(marker[0])
-            visited_col.add(marker[1])
+        for col in cols:
+            for i in range(len(matrix)):
+                matrix[i][col] = 0
