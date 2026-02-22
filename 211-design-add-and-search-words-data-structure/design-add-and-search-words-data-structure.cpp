fd42d804a71cc26d191798915e4ce139d2ed0bd2
Amrit Srivastava <amrit.madhav.srivastava@gmail.com>
2026-02-22 14:19:18 -0500

    Time: 332 ms (70.06%) | Memory: 563.3 MB (51.30%) - LeetSync

---
+++ 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.cpp
@@ -0,0 +1,69 @@
+class WordDictionary {
+private:
+    struct Node {
+        Node* children[26];
+        bool isEndOfWord;
+
+        Node() {
+            isEndOfWord = false;
+            for (int i = 0; i < 26; i++) {
+                children[i] = nullptr;
+            }
+        }
+    };
+
+    Node* root;
+
+    bool dfs(const string& word, int index, Node* curr) {
+            if (index == word.length()) {
+                return curr->isEndOfWord;
+            }
+    
+            char c = word[index];
+    
+            if (c == '.') {
+                for (int i = 0; i < 26; i++) {
+                    if (curr->children[i] != nullptr) {
+                        if (dfs(word, index + 1, curr->children[i])) {
+                            return true;
+                        }
+                    }
+                }
+                return false;
+            } else {
+                int idx = c - 'a';
+                if (curr->children[idx] == nullptr) {
+                    return false;
+                }
+                return dfs(word, index + 1, curr->children[idx]);
+            }
+        }
+    
+public:
+    WordDictionary() {
+        root = new Node();
+    }
+    
+    void addWord(string word) {
+        auto currentNode = this->root;
+        for (auto character : word) {
+            int i = character - 'a';
+            if (!currentNode->children[i]) {
+                currentNode->children[i] = new Node();
+            }
+            currentNode = currentNode->children[i];
+        }
+        currentNode->isEndOfWord = true;
+    }
+    
+    bool search(string word) {
+        return dfs(word, 0, this->root);
+    }
+};
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary* obj = new WordDictionary();
+ * obj->addWord(word);
+ * bool param_2 = obj->search(word);
+ */
