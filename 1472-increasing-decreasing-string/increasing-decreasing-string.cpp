f303537330aa0b7a0bad7842cb5e0fac61d73eff
Amrit Srivastava <amrit.madhav.srivastava@gmail.com>
2026-02-23 20:29:37 -0500

    Time: 4 ms (36.46%) | Memory: 10.1 MB (43.04%) - LeetSync

--- 1472-increasing-decreasing-string/increasing-decreasing-string.cpp
+++ 1472-increasing-decreasing-string/increasing-decreasing-string.cpp
@@ -10,14 +10,14 @@ public:
         while (remaining > 0) {
             for (int i = 0; i < 26; ++i) {
                 if (counts[i] > 0) {
-                    result += (char)(i + 'a');
+                    result += static_cast<char>(i + 'a');
                     counts[i]--;
                     remaining--;
                 }
             }
             for (int i = 25; i >= 0; --i) {
                 if (counts[i] > 0) {
-                    result += (char)(i + 'a');
+                    result += static_cast<char>(i + 'a');
                     counts[i]--;
                     remaining--;
                 }
