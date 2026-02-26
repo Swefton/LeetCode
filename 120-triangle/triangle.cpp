85fc1a7d2940044e1d5b46abf3054be6acd31126
Amrit Srivastava <amrit.madhav.srivastava@gmail.com>
2026-02-26 12:09:20 -0500

    Time: 1 ms (19.36%) | Memory: 12.6 MB (90.49%) - LeetSync

---
+++ 120-triangle/triangle.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        for (int row = triangle.size()-2; row >= 0; row--) {
+            cout << row << " \n";
+            for (size_t i = 0; i < triangle[row].size(); ++i) {
+                triangle[row][i] += min(triangle[row+1][i], triangle[row+1][i+1]);
+            }
+        }
+
+        return triangle.front().front();
+    }
+};
