66da96003afcd4c99e99503ffcfee0053039b130
Amrit Srivastava <amrit.madhav.srivastava@gmail.com>
2026-03-17 18:28:44 -0400

    Time: 38 ms (52.13%) | Memory: 90.3 MB (35.64%) - LeetSync

--- 1845-largest-submatrix-with-rearrangements/largest-submatrix-with-rearrangements.cpp
+++ 1845-largest-submatrix-with-rearrangements/largest-submatrix-with-rearrangements.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     int largestSubmatrix(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-        vector<int> prevHeights = matrix[0];
+        int m = matrix.size(), n = matrix[0].size();
         int maxArea = 0;
+        vector<int> heights(n, 0);
 
         for (int i = 0; i < m; ++i) {
-                for (int j = 0; j < n; ++j) {
-                    if (i > 0) {
-                        prevHeights[j] = matrix[i][j] ? prevHeights[j] + 1 : 0;
-                    }
-                }
-                
-                auto temp = prevHeights;
-                ranges::sort(temp, std::greater{});
-
-                for (int pos = 0; pos < n; ++pos) maxArea = max(maxArea, temp[pos] * (pos+1));
+            for (int j = 0; j < n; ++j) {
+                if (matrix[i][j] == 1) heights[j]++;
+                else heights[j] = 0;
             }
-        
+
+            vector<int> sortedHeights = heights; 
+            sort(sortedHeights.begin(), sortedHeights.end(), greater<int>());
+
+            for (int j = 0; j < n; ++j) {
+                if (sortedHeights[j] == 0) break;
+                maxArea = max(maxArea, sortedHeights[j] * (j + 1));
+            }
+        }
         return maxArea;
     }
 };
