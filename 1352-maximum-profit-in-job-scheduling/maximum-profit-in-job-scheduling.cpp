fc9c3ec91aab8463cf3b673e8bcc36f4e04fb4ba
Amrit Srivastava <amrit.madhav.srivastava@gmail.com>
2026-03-16 23:39:30 -0400

    Time: 24 ms (89.90%) | Memory: 76.1 MB (96.49%) - LeetSync

--- 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.cpp
+++ 1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.cpp
@@ -1,29 +1,34 @@
 class Solution {
-    public:
-        int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
-            struct Job {
-                int start, end, profit;
-            };
+public:
+    int jobScheduling(vector<int>& startTime, vector<int>& endTime,
+                      vector<int>& profit) {
+        struct Job {
+            int start, end, profit;
+        };
 
-            int n = startTime.size();
+        int n = startTime.size();
 
-            vector<Job> jobs(n);
-            for (int i = 0; i < n; ++i) {
-                jobs[i] = {startTime[i], endTime[i], profit[i]};
-            }
-
-            sort(jobs.begin(), jobs.end(), [](const Job& a, const Job& b){
-                return a.end < b.end;
-            });
-
-            vector<int> bestScores(n + 1, 0);
-
-            for (int i = 1; i <= n; ++i) {
-               int prevIdx = upper_bound(jobs.begin(), jobs.begin() + i - 1, jobs[i-1].start, [](int val, const Job& j) { return val < j.end; }) - jobs.begin(); 
-
-                bestScores[i] = max(bestScores[i-1], jobs[i-1].profit + bestScores[prevIdx]); 
-            }
-
-            return bestScores.back();
+        vector<Job> jobs(n);
+        for (int i = 0; i < n; ++i) {
+            jobs[i] = {startTime[i], endTime[i], profit[i]};
         }
+
+        sort(jobs.begin(), jobs.end(),
+             [](const Job& a, const Job& b) { return a.end < b.end; });
+
+        vector<int> bestScores(n + 1, 0);
+
+        for (int i = 1; i <= n; ++i) {
+            int prevIdx =
+                upper_bound(jobs.begin(), jobs.begin() + i - 1,
+                            jobs[i - 1].start,
+                            [](int val, const Job& j) { return val < j.end; }) -
+                jobs.begin();
+
+            bestScores[i] = max(bestScores[i - 1],
+                                jobs[i - 1].profit + bestScores[prevIdx]);
+        }
+
+        return bestScores.back();
+    }
 };
