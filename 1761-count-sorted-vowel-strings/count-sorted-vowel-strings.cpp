75bd7cc256000baf93784e9e56299824d0dba5e9
Amrit Srivastava <amrit.madhav.srivastava@gmail.com>
2026-03-19 00:38:05 -0400

    Time: 0 ms (100.00%) | Memory: 7.9 MB (57.01%) - LeetSync

--- 1761-count-sorted-vowel-strings/count-sorted-vowel-strings.cpp
+++ 1761-count-sorted-vowel-strings/count-sorted-vowel-strings.cpp
@@ -7,15 +7,14 @@ public:
 
 private:
     int nCr(int n, int r) {
-        // No valid combinations if r is greater than n
-        if (r > n) 
-            return 0;
-    
-        // Base case: only one way to choose 0 or all elements
-        if (r == 0 || r == n) 
-            return 1;
-    
-        // include or exclude current element
-        return nCr(n - 1, r - 1) + nCr(n - 1, r);
+        if (r > n) return 0;
+        if (r == 0 || r == n) return 1;
+        if (r > n / 2) r = n - r;
+
+        long long res = 1;
+        for (int i = 1; i <= r; ++i) {
+            res = res * (n - i + 1) / i;
+        }
+        return (int)res;
     }
 };
