0b5f0c4ac2da3d70860b3afc28ab8448269f7f81
Amrit Srivastava <amrit.madhav.srivastava@gmail.com>
2026-03-19 01:32:30 -0400

    Time: 0 ms (100.00%) | Memory: 7.8 MB (80.22%) - LeetSync

--- 1761-count-sorted-vowel-strings/count-sorted-vowel-strings.cpp
+++ 1761-count-sorted-vowel-strings/count-sorted-vowel-strings.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     int countVowelStrings(int n) {
-        return nCr(n+4, n);
-    }
+        vector<int> dp(5, 1);
 
-
-private:
-    int nCr(int n, int r) {
-        if (r > n) return 0;
-        if (r == 0 || r == n) return 1;
-        if (r > n / 2) r = n - r;
-
-        long long res = 1;
-        for (int i = 1; i <= r; ++i) {
-            res = res * (n - i + 1) / i;
+        for (int i = 1; i < n; ++i) {
+            for (int j = 3; j >= 0; --j) {
+                dp[j] += dp[j + 1];
+            }
         }
-        return (int)res;
+
+        int total = 0;
+        for (int count : dp) total += count;
+        return total;
     }
 };
