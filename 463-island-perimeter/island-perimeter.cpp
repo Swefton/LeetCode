143ce1a9aeee6d081bc9fabe43020d9808523e75
Amrit Srivastava <amrit.madhav.srivastava@gmail.com>
2026-03-22 00:17:03 -0400

    Time: 0 ms (100.00%) | Memory: 102.5 MB (34.31%) - LeetSync

--- 463-island-perimeter/island-perimeter.cpp
+++ 463-island-perimeter/island-perimeter.cpp
@@ -7,9 +7,10 @@ public:
             for (int j = 0; j < grid[0].size(); ++j) {
                 if (grid[i][j]) {
                     int sides = 4;
-                    int dirs[4][2] = {{1,0},{-1,0},{0,-1},{0,1}};
+                    int dirs[2][2] = {{-1,0},{0,-1}};
+                    
                     for (auto [dirY, dirX] : dirs) {
-                        if (i+dirY >= 0 && i+dirY < grid.size() && j+dirX >= 0 && j+dirX < grid[0].size() && grid[i+dirY][j+dirX]) --sides;
+                        if (i+dirY >= 0 && i+dirY < grid.size() && j+dirX >= 0 && j+dirX < grid[0].size() && grid[i+dirY][j+dirX]) sides -= 2;
                     }
 
                     perimeter += sides;
