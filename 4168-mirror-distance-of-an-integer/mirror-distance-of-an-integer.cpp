9517a2b6081363ae5c812d74243f677f9ebd90c9
Amrit Srivastava <amrit.madhav.srivastava@gmail.com>
2026-04-18 19:52:35 -0400

    Time: 0 ms (100.00%) | Memory: 8.4 MB (99.29%) - LeetSync

---
+++ 4168-mirror-distance-of-an-integer/mirror-distance-of-an-integer.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int mirrorDistance(int n) {
+        int reverse = 0, copy = n;
+
+        while (copy) {
+            reverse += copy % 10;
+            copy /= 10;
+            reverse *= copy ? 10 : 1;
+        }
+
+        return abs((long)n - (long)(reverse));
+    }
+};
