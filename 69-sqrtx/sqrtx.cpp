acf2fdcc24771786047105094aae50157a5f7a20
Amrit Srivastava <amrit.madhav.srivastava@gmail.com>
2026-04-17 21:27:57 -0400

    Time: 0 ms (100.00%) | Memory: 8.6 MB (13.58%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -2,16 +2,10 @@ class Solution {
 public:
     int mySqrt(int x) {
         if (x < 2) return x;
-        
-        int l = 1, r = x;
-        while (l <= r) {
-            int m = l + (r - l) / 2;
-            
-            if (m == x / m) return m;
-            else if (m < x / m) l = m + 1;
-            else r = m - 1;
-        }
-        
-        return r;
+
+        auto range = std::views::iota(1LL, (long long)x + 1);
+        auto it = std::ranges::partition_point(range, [x](long long m) { return m * m <= (long long)x; });
+
+        return (int)(*std::ranges::prev(it));
     }
 };
