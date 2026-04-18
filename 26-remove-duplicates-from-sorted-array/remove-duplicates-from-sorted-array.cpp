5b1729ed1841d128669f88ae47b228d095781160
Amrit Srivastava <amrit.madhav.srivastava@gmail.com>
2026-04-17 21:49:38 -0400

    Time: 0 ms (100.00%) | Memory: 22.6 MB (79.49%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -4,10 +4,7 @@ public:
         int j = 0;
 
         for (int i = 0; i < nums.size(); ++i) {
-            if (nums[i] != nums[j]) {
-                j++;
-                swap(nums[i], nums[j]);
-            }
+            if (nums[i] != nums[j]) swap(nums[i], nums[++j]);
         }
 
         return j+1;
