db214837ad0fad7e798a3661c8ac6a9356a90c23
Amrit Srivastava <amrit.madhav.srivastava@gmail.com>
2026-04-17 21:49:10 -0400

    Time: 0 ms (100.00%) | Memory: 22.7 MB (51.71%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int j = 0;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] != nums[j]) {
+                j++;
+                swap(nums[i], nums[j]);
+            }
+        }
+
+        return j+1;
+    }
+};
